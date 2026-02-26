cf7102bdf2da50c35d0b3fccf21521fc24d96d7c
Vanessa Ayertey <ayerteyvanessa@gmail.com>
2026-02-26 15:20:34 +0000

    Add Dockerfile

--- backend/project/src/main/java/com/backend/project/ProjectApplication.java
+++ backend/project/src/main/java/com/backend/project/ProjectApplication.java
@@ -1,6 +1,5 @@
 package com.backend.project;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
